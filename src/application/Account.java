f05c37c26e20dd398e337dd25c5b72a174ba6cd6
gregfyl <fylzj2003@gmail.com>
2025-09-04 21:32:49 -0400

    Account Class

---
+++ src/application/Account.java
@@ -0,0 +1,108 @@
+package application;
+
+/**
+ * Account Class
+ * This is an abstract class that defines the common features of all account types.
+ */
+
+public abstract class Account {
+
+	/** Holder of the account. */
+	private Profile holder;
+	/** Balance of the account. */
+	private double balance;
+	/** Date of the account opened. */
+	private Date dateOpen;
+
+	/**
+	 * Initialize the account by holder, balance, and dateOpen.
+	 * @param holder   Holder of the account.
+	 * @param balance  Balance of the account.
+	 * @param dateOpen Date of the account opened.
+	 */
+	public Account(Profile holder, double balance, Date dateOpen) {
+		this.holder = holder;
+		this.balance = balance;
+		this.dateOpen = dateOpen;
+	}
+
+	/**
+	 * Decrease the balance by amount.
+	 * @param amount Amount to be decreased.
+	 */
+	public void debit(double amount) {
+		balance -= amount;
+	}
+
+	/**
+	 * Increase the balance by amount.
+	 * @param amount Amount to be increase.
+	 */
+	public void credit(double amount) {
+		balance += amount;
+	}
+
+	/**
+	 * Convert the account to a string.
+	 * @return The account in string format.
+	 */
+	@Override
+	public String toString() {
+		return holder.toString() + "* $" + String.format("%,.2f", balance) + "*" + dateOpen;
+	}
+
+	/**
+	 * Calculate the monthly interest of the account.
+	 * @return Monthly interest of the account.
+	 */
+	public abstract double monthlyInterest();
+
+	/**
+	 * Calculate the monthly fee of the account.
+	 * @return Monthly fee of the account.
+	 */
+	public abstract double monthlyFee();
+
+	/**
+	 * Determine if the account matches with another account.
+	 * @param account Another account that is to compared with.
+	 * @return True if the account with another account.<br>
+	 *         False otherwise.
+	 */
+	@Override
+	public boolean equals(Object account) {
+		if (account == null)
+			return false;
+		if (this.getClass().toString().equals(account.getClass().toString())) {
+			if (this.getHolder().toString().equals(((Account) account).getHolder().toString())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Get the holder of the account.
+	 * @return Holder of the account.
+	 */
+	public Profile getHolder() {
+		return holder;
+	}
+
+	/**
+	 * Get the balance of the account.
+	 * @return Balance of the account.
+	 */
+	public double getBalance() {
+		return balance;
+	}
+
+	/**
+	 * Get the date of the account opened.
+	 * @return Date of the account opened.
+	 */
+	public Date getDateOpen() {
+		return dateOpen;
+	}
+
+}
